10d23fa4622972709ac9e0a17675e486bf402ce0
albam <albamamo07@gmail.com>
2024-05-08 13:32:26 +0200

    Añadir libreria en el proyecto

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,27 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Main {
+    //10.94.255.99 Santi PhpMyAdmin 3336
+    //nba_2023-24
+    //root mas
+
+    //192.168.56.103 BD mysql 3306
+    //perepi pastanaga
+    //nba
+    public static final String URL = "jdbc:mysql://192.168.56.103:3306/nba";
+    public static final String USER = "perepi";
+    public static final String PASSWORD = "pastanaga";
     public static void main(String[] args) {
 
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.56.103:3306/rrhh", "perepi", "pastanaga");
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
 
             Statement statement = connection.createStatement();
 
-            ResultSet resultSet = statement.executeQuery("select * from empleats");
+            ResultSet resultSet = statement.executeQuery("select * from equips");
 
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("empleat_id"));
+                System.out.println(resultSet.getString("id"));
             }
         } catch (Exception e){
             e.printStackTrace();
