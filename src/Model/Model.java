0cc8f7de7cd3e5b9514362a554d8865c55bfdfc7
albam <albamamo07@gmail.com>
2024-05-14 12:41:50 +0200

    Model funciones ejs

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -2,7 +2,18 @@ package Model;
 
 public class Model {
 
+    //1. Llistar tots els jugadors d'un equip
     public static void llistarJugadorsEquip(){
+
+    }
+
+    //2. Calcular la mitjana de punts, rebots, assistències, ... d'un jugador
+    public static void calcularMitjanaJugador(){
+
+    }
+
+    //3. Llistar tots els partits jugats per un equip amb el seu resultat
+    public static void llistarPartitsJugats(){
 
     }
 }
