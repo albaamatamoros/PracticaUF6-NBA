a699a4d0e76fa5f4ae0e31fb23172f7c52d56b54
albam <albamamo07@gmail.com>
2024-05-15 12:33:03 +0200

    no he acabao pero guardo 'save'

--- src/Model/Partit/PartitDAO.java
+++ src/Model/Partit/PartitDAO.java
@@ -1,10 +1,20 @@
 package Model.Partit;
+import Model.Model;
 import Model.DAO;
+
+import java.sql.PreparedStatement;
+import java.util.Collections;
 
 public class PartitDAO implements DAO<Partit> {
 
     @Override
     public boolean insertar(Partit partit) {
+        Collections connexio = null;
+        PreparedStatement sentencia = null;
 
+        try {
+            connexio = Model.getConnection();
+            sentencia = connexio.prepareStatement("INSERT INTO ");
+        }
     }
 }
