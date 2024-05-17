83fc816e01aa45031205a0e8ed780538e8fdaeed
albam <albamamo07@gmail.com>
2024-05-17 12:13:20 +0200

    Guardar para mergeÃ

--- src/Model/EstadisticaJugador/EstadisticaJugadorDAO.java
+++ src/Model/EstadisticaJugador/EstadisticaJugadorDAO.java
@@ -1,9 +1,8 @@
 package Model.EstadisticaJugador;
 
-import Model.Jugador.Jugador;
+import Model.DAO;
+import Model.Equip.Equip;
 import Model.Model;
-import Model.Partit.Partit;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -17,24 +16,25 @@ public class EstadisticaJugadorDAO implements DAO<EstadisticaJugador>{
 
         try {
             connexio = Model.getConnection();
-            sentencia = connexio.prepareStatement("INSERT INTO estadistiques_jugadors (assistencies,bloqueigs,equip_id,jugador_id,minuts_jugats,partit_id,punts,rebots_defensius,rebots_ofensius,robades,tirs_anotats,tirs_lliures_anotats,tirs_lliures_tirats,tirs_tirats,tirs_triples_anotats,tirs_triples_tirats) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+            sentencia = connexio.prepareStatement("INSERT INTO estadistiques_jugadors () VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 
-            sentencia.setInt(1,estadisticaJugador.getAssistencies());
-            sentencia.setInt(2,estadisticaJugador.getBloqueigs());
-            sentencia.setInt(3,estadisticaJugador.getEquipId());
+
             sentencia.setInt(4,estadisticaJugador.getJugadorId());
-            sentencia.setFloat(5,estadisticaJugador.getMinutsJugats());
+            sentencia.setInt(3,estadisticaJugador.getEquipId());
             sentencia.setInt(6,estadisticaJugador.getPartitId());
-            sentencia.setInt(7,estadisticaJugador.getRebotsDefensius());
-            sentencia.setInt(8,estadisticaJugador.getRebotsDefensius());
-            sentencia.setInt(9,estadisticaJugador.getRebotsOfensius());
-            sentencia.setInt(10,estadisticaJugador.getRobades());
+            sentencia.setFloat(5,estadisticaJugador.getMinutsJugats());
+            sentencia.setInt(1,estadisticaJugador.getPunts());
             sentencia.setInt(11,estadisticaJugador.getTirsAnotats());
-            sentencia.setInt(12,estadisticaJugador.getTirsLliuresAnotats());
-            sentencia.setInt(13,estadisticaJugador.getTirsLliuresTirats());
             sentencia.setInt(14,estadisticaJugador.getTirsTirats());
             sentencia.setInt(15,estadisticaJugador.getTirsTriplesAnotats());
             sentencia.setInt(16,estadisticaJugador.getTirsTriplesTirats());
+            sentencia.setInt(12,estadisticaJugador.getTirsLliuresAnotats());
+            sentencia.setInt(13,estadisticaJugador.getTirsLliuresTirats());
+            sentencia.setInt(9,estadisticaJugador.getRebotsOfensius());
+            sentencia.setInt(8,estadisticaJugador.getRebotsDefensius());
+            sentencia.setInt(7,estadisticaJugador.getAssistencies());
+            sentencia.setInt(10,estadisticaJugador.getRobades());
+            sentencia.setInt(2,estadisticaJugador.getBloqueigs());
 
         }catch (SQLException e) {
             return false;
@@ -51,4 +51,13 @@ public class EstadisticaJugadorDAO implements DAO<EstadisticaJugador>{
             }
         }
     }
+
+    @Override
+    public boolean actualitzar(Equip equip) {return true;}
+
+    @Override
+    public boolean esborrar(Equip equip) {return false;}
+
+    @Override
+    public int count() {return 1;}
 }
